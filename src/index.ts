d16ac7b632e0a87b4b44bfa5a6c44668f53dfc9d
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-09 17:37:27 +0000

    Imported CharactersCollection into index.ts and tested functionality with test string

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 import { Sorter } from "./Sorter";
 
+// Sorting array of numbers
 const numbersCollection = new NumbersCollection([10, 3, -5, 0])
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const arraySorter = new Sorter(numbersCollection);
+arraySorter.sort();
 console.log(numbersCollection.data);
+
+// Sorting string
+const charactersCollection = new CharactersCollection('Sunny');
+const charactersSorter = new Sorter(charactersCollection);
+charactersSorter.sort();
+console.log(charactersCollection.data);
