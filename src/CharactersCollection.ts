139360270816de180b132e7476413658286324f8
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-09 17:34:20 +0000

    Created new CharactersCollection class with relevant properties to work with Sorter

---
+++ src/CharactersCollection.ts
@@ -0,0 +1,26 @@
+export class CharactersCollection {
+  constructor(public data: string) {}
+
+  // Declare getter to enable referencing length in the way we're used to (without calling the function)
+  // Declare get in front of the function - can't use arrow function syntax
+  get length(): number {
+    return this.data.length;
+  };
+
+  compare = (leftIndex: number, rightIndex: number): boolean => {
+    // Return true if left char is greater than right char, if not then return false
+    // Need to convert to lowercase to enable ASCII code comparison to work properly
+    return this.data[leftIndex].toLowerCase() > this.data[rightIndex].toLowerCase();
+  };
+
+  swap = (leftIndex: number, rightIndex: number): void => {
+    // Logic to swap the left and right char - split into array, compare and swap, join the array to a string
+    const characters = this.data.split('');
+
+    const leftHand = characters[leftIndex];
+    characters[leftIndex] = characters[rightIndex];
+    characters[rightIndex] = leftHand;
+
+    this.data = characters.join('');
+  };
+}
