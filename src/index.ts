96fce333aeeacaeb87a5fff7ad6937d3abc29aeb
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-09 17:00:18 +0000

    Moved Sorter class into separate file and imported into index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,4 @@
-class Sorter {
-  // collection must be an array of numbers - the variable is initialised automatically when it is passed in
-  constructor(public collection: number[]) {}
-
-  sort = (): void => {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      // Ignore the right-most element on each progressive loop
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          // If the element on the left is greater, swap it with the element on the right
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j+1];
-          this.collection[j+1] = leftHand;
-        }
-      }
-    }
-  }
-}
+import { Sorter } from "./Sorter";
 
 const sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
