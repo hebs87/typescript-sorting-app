95bceec0f230687023471f4f907893a83b55bcd6
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-09 17:25:31 +0000

    Created new Sortable interface to make Sorter generic to any data structure

--- src/Sorter.ts
+++ src/Sorter.ts
@@ -1,12 +1,19 @@
-import { NumbersCollection } from "./NumbersCollection";
+// Define an interface to make the Sorter class generic and work with any data structure, as long as its sorting
+// class has the following properties
+interface Sortable {
+  length: number;
+  compare: (leftIndex: number, rightIndex: number) => boolean;
+  swap: (leftIndex: number, rightIndex: number) => void;
+}
 
 export class Sorter {
   // collection must be an array of numbers - the variable is initialised automatically when it is passed in
-  constructor(public collection: NumbersCollection) {}
+  constructor(public collection: Sortable) {}
 
   sort = (): void => {
     const { length } = this.collection;
 
+    // Bubble sort algorithm
     for (let i = 0; i < length; i++) {
       // Ignore the right-most element on each progressive loop
       for (let j = 0; j < length - i - 1; j++) {
