8b22e8eb950c106a5471e25bfe7ccbc4df76ecf1
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2021-01-10 00:27:18 +0000

    Import LinkedList into index.ts and tested with linked list

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
 // Sorting array of numbers
@@ -13,3 +14,13 @@ const charactersCollection = new CharactersCollection('Sunny');
 const charactersSorter = new Sorter(charactersCollection);
 charactersSorter.sort();
 console.log(charactersCollection.data);
+
+// Sorting linked lists
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const linkedListSorter = new Sorter(linkedList);
+linkedListSorter.sort();
+linkedList.print();
